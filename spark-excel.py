94bd02f1844f2b7674ef9f99604922c9de91b7ae
deepu652728 <30902833+deepu652728@users.noreply.github.com>
2021-05-19 21:01:59 +0530

    Add files via upload

---
+++ spark-excel.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    print("Real-Time Data Pipeline Started ...")
+
+spark = SparkSession \
+    .builder \
+    .appName("Real-Time Data Pipeline Demo") \
+    .master("local[*]") \
+     .getOrCreate()
+     #.config("spark.jars", "file:///G://jars//spark-excel_2.11-0.13.1.jar") \
+
+    # .config("spark.executor.extraClassPath", "file:///G://jars//spark-excel_2.11-0.8.2.jar") \
+    # .config("spark.executor.extraLibrary", "file:///G://jars//spark-excel_2.11-0.8.2.jar") \
+    # .config("spark.driver.extraClassPath", "file:///G://jars//spark-excel_2.11-0.8.2.jar") \
+   # .getOrCreate()
+
+print("Session Created")
+
+df = spark.read.format("com.crealytics.spark.excel").\
+    option("header", "true").load("C:/Users/admin/Desktop/Book1.xlsx")
+
+print(df.show())
